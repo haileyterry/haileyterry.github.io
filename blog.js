d5f5029e20730c3bea25290edb4b027ef57f0d8e
haileyterry <41652951+haileyterry@users.noreply.github.com>
2018-07-25 09:59:33 -0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,10 +1,11 @@
-$("#slideshow > div:gt(0)").hide();
+function enlarge(word){
+  word.style.fontSize = "40px";
+}
 
-setInterval(function() { 
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  3000);
+function smaller(word){
+  word.style.fontSize = "30px";
+}
+
+function next(photo){
+  
+}
