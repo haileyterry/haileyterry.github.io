5af09890ad7d4123ecebebcd6c6f20a1ab1e2997
haileyterry <41652951+haileyterry@users.noreply.github.com>
2018-07-25 15:48:52 -0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -6,15 +6,4 @@ function smaller(word){
   word.style.fontSize = "30px";
 }
 
-function next(photo){
 
-}
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
