6420c38bc9535e94a130fe1b56df84e796f71ce8
haileyterry <41652951+haileyterry@users.noreply.github.com>
2018-07-25 09:57:28 -0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,31 +1,10 @@
-body {text-align: center;}
-body {background-color: 	rgb(230,230,250);}
-  h1{
-    font-family: 'Merienda', cursive;
-    font-size: 30pt;
-    }
-  p{
-    font-family: 'Indie Flower', cursive;
-    font-size: 12pt;
-    }
-  h3, h4{
-    font-family: 'PT Sans', sans-serif;
-    font-size: 20pt;
-    }
+$("#slideshow > div:gt(0)").hide();
 
-    #slideshow { 
-        margin: 50px auto; 
-        position: relative; 
-        width: 240px; 
-        height: 240px; 
-        padding: 10px; 
-        box-shadow: 0 0 20px rgba(0,0,0,0.4); 
-    }
-    
-    #slideshow > div { 
-        position: absolute; 
-        top: 10px; 
-        left: 10px; 
-        right: 10px; 
-        bottom: 10px; 
-    }
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
