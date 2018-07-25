3db3514834e287593d7de6801935712d842f94f4
haileyterry <41652951+haileyterry@users.noreply.github.com>
2018-07-25 09:57:06 -0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,11 +1,31 @@
-function enlarge(word){
-  word.style.fontSize = "40px";
-}
+body {text-align: center;}
+body {background-color: 	rgb(230,230,250);}
+  h1{
+    font-family: 'Merienda', cursive;
+    font-size: 30pt;
+    }
+  p{
+    font-family: 'Indie Flower', cursive;
+    font-size: 12pt;
+    }
+  h3, h4{
+    font-family: 'PT Sans', sans-serif;
+    font-size: 20pt;
+    }
 
-function smaller(word){
-  word.style.fontSize = "30px";
-}
-
-function next(photo){
-  
-}
+    #slideshow { 
+        margin: 50px auto; 
+        position: relative; 
+        width: 240px; 
+        height: 240px; 
+        padding: 10px; 
+        box-shadow: 0 0 20px rgba(0,0,0,0.4); 
+    }
+    
+    #slideshow > div { 
+        position: absolute; 
+        top: 10px; 
+        left: 10px; 
+        right: 10px; 
+        bottom: 10px; 
+    }
