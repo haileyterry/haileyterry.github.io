71e30b148f0f5cee3206535fcc67a90369b2932a
haileyterry <41652951+haileyterry@users.noreply.github.com>
2018-07-26 12:59:30 -0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -6,4 +6,9 @@ function smaller(word){
   word.style.fontSize = "30px";
 }
 
-
+function makeGrey(x){
+  x.style.backgroundColor="#999999";
+}
+function makeBlack(x){
+  x.style.backgroundColor="black";
+}
